7a5d1af1d640927999fce3e229f74889e9a1421c
Andrej Snitko <snitko@seznam.cz>
2018-03-15 16:35:04 +0100

    DB migrated to Postgres

--- knards/knards/settings.py
+++ knards/knards/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'rest_framework',
     'api'
 ]
@@ -76,9 +77,16 @@ WSGI_APPLICATION = 'knards.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'knards',
+        'USER': 'percolant',
+        'PASSWORD': '200Hm1Hx',
+        'HOST': ''
     }
 }
 
